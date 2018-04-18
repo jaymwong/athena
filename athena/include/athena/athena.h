920358833c5124fcf23874068ff024502b8354a8
Jay M. Wong <jay@southieautonomyworks.com>
2018-04-17 23:27:43 -0400

    Adding top level header for athena

---
+++ athena/include/athena/athena.h
@@ -0,0 +1,10 @@
+#ifndef ATHENA_H
+#define ATHENA_H
+
+#include <athena/pointcloud/conversions.h>
+#include <athena/pointcloud/utils.h>
+#include <athena/transform/conversions.h>
+#include <athena/visualization/utils.h>
+
+
+#endif
